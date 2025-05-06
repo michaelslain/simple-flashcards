3cbecf266f703260e374f3c7090abdefe27cf730
michaelslain <misha.slain@icloud.com>
2025-05-06 16:48:33 -0700

    hebrew and russian font support for inputs

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,32 @@
-import { FC, InputHTMLAttributes } from 'react'
+import { FC, InputHTMLAttributes, useState } from 'react'
 import styles from './Input.module.scss'
+import fontStyles from './LanguageFont.module.scss'
+import checkLanguage from '../util/checkLanguage'
 
 const Input: FC<InputHTMLAttributes<HTMLInputElement>> = ({
     className = '',
     ...props
 }) => {
-    return <input className={`${styles.input} ${className}`} {...props} />
+    const [value, setValue] = useState(props.value?.toString() || '')
+    const language = checkLanguage(value)
+    const fontClass =
+        language === 'hebrew'
+            ? fontStyles.hebrew
+            : language === 'russian'
+            ? fontStyles.russian
+            : fontStyles.english
+
+    return (
+        <input
+            className={`${styles.input} ${fontClass} ${className}`}
+            {...props}
+            value={value}
+            onChange={e => {
+                setValue(e.target.value)
+                props.onChange?.(e)
+            }}
+        />
+    )
 }
 
 export default Input
